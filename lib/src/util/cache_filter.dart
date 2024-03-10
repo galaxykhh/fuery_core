b2f882a79ee12654ff3ec8d53b2dd6be5d96c92a
hyeonghan <galaxykhh@gmail.com>
2024-03-10 19:19:43 +0900

    feat: add fuerykey compare functions

--- lib/src/util/cache_filter.dart
+++ lib/src/util/cache_filter.dart
@@ -13,19 +13,81 @@ mixin CacheFilter<T extends Cacheable> {
 
     assert(key.isNotEmpty, 'key should not be empty');
 
-    return items.entries.where((item) {
-      if (exact == true) return item.value.key == key;
+    return items.entries
+        .where((item) {
+          if (exact == true) {
+            return _compare(
+              key: key,
+              targetKey: item.value.key,
+              exact: exact,
+            );
+          }
 
-      return (
-        _hasSameOrLongerKeyLength(key: key, target: item.value)
-      );
-    }).map((item) => item.value).toList();
+          return _hasSameOrLongerKeyLength(
+                key: key,
+                targetKey: item.value.key,
+              ) &&
+              _compare(
+                key: key,
+                targetKey: item.value.key,
+              );
+        })
+        .map((item) => item.value)
+        .toList();
+  }
+
+  bool _hasSameKeyLength({
+    required FueryKey key,
+    required FueryKey targetKey,
+  }) {
+    return key.length == targetKey.length;
+  }
+
+  bool _hasLongerKeyLength({
+    required FueryKey key,
+    required FueryKey targetKey,
+  }) {
+    return key.length <= targetKey.length;
   }
 
   bool _hasSameOrLongerKeyLength({
     required FueryKey key,
-    required T target,
+    required FueryKey targetKey,
   }) {
-    return key.length >= target.key.length;
+    return _hasSameKeyLength(
+          key: key,
+          targetKey: targetKey,
+        ) ||
+        _hasLongerKeyLength(
+          key: key,
+          targetKey: targetKey,
+        );
+  }
+
+  bool _shouldStringify(dynamic target) {
+    return (target is! String && target is! int && target is! double && target is! bool);
+  }
+
+  bool _compare({
+    required FueryKey key,
+    required FueryKey targetKey,
+    bool? exact,
+  }) {
+    if (exact == true && !_hasSameKeyLength(key: key, targetKey: targetKey)) {
+      return false;
+    }
+
+    for (final (int, dynamic) item in key.indexed.toList()) {
+      final dynamic keyItem = item.$2;
+      final dynamic targetItem = targetKey[item.$1];
+      final bool isSameType = targetItem.runtimeType == keyItem.runtimeType;
+      final bool isSame = isSameType && _shouldStringify(targetItem) ? targetItem.toString() == keyItem.toString() : targetItem == keyItem;
+
+      if (isSame) continue;
+
+      return false;
+    }
+
+    return true;
   }
 }
